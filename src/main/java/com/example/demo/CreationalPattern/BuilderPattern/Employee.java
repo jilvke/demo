845c04982b487829683b8cd4f142964555c88c4f
jilvke <1643621827@qq.com>
2019-12-04 15:54:32 +0800

    单例模式
    Signed-off-by: jilvke <1643621827@qq.com>

---
+++ src/main/java/com/example/demo/CreationalPattern/BuilderPattern/Employee.java
@@ -0,0 +1,25 @@
+package com.example.demo.CreationalPattern.BuilderPattern;
+
+/**
+ * @ClassName : Employee
+ * @Description: TODO
+ * @Author: jilvke
+ * @CreateDate: 2019/12/04
+ * @Version: 1.0
+ */
+public class Employee {
+    private CakeBuilder cakeBuilder;
+
+    public void setCakeBuilder(CakeBuilder cakeBuilder) {
+        this.cakeBuilder = cakeBuilder;
+    }
+
+    public Cake makeCake(String cakeName, String cakeGiftBox, String cakeTaste, String cakePoster, String cakeEvaluation) {
+        this.cakeBuilder.buildCakeEvaluation(cakeEvaluation);
+        this.cakeBuilder.buildCakeName(cakeName);
+        this.cakeBuilder.buildCakeGiftBox(cakeGiftBox);
+        this.cakeBuilder.buildCakeTaste(cakeTaste);
+        this.cakeBuilder.buildCakePoster(cakePoster);
+        return this.cakeBuilder.makeCake();
+    }
+}
---
+++ src/test/java/com/example/demo/CreationalPattern/SingletonPattern/HungrySingletonTest.java
@@ -0,0 +1,39 @@
+package com.example.demo.CreationalPattern.SingletonPattern;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+@Slf4j
+class HungrySingletonTest {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        HungrySingleton hungrySingleton = HungrySingleton.getInstance();
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("singleton"));
+        oos.writeObject(hungrySingleton);
+
+        File file = new File("singleton");
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+
+        HungrySingleton newHungrySingleton = (HungrySingleton) ois.readObject();
+
+        log.info("结果 {}", hungrySingleton);
+        log.info("结果 {}", newHungrySingleton);
+        log.info("对比结果 {}", hungrySingleton == newHungrySingleton);
+
+
+        Class objectClass = HungrySingleton.class;
+        Constructor constructor = objectClass.getDeclaredConstructor();
+        constructor.setAccessible(true); // 强行打开构造器权限
+        HungrySingleton instance = HungrySingleton.getInstance();
+        HungrySingleton newInstance = (HungrySingleton) constructor.newInstance();
+
+        log.info("结果{}", instance);
+        log.info("结果{}", newInstance);
+        log.info("比较结果{}", newInstance == instance);
+
+    }
+
+
+}
