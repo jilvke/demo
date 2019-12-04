f9af558bdd878590e1751c1dbfd21abf36a5d393
jilvke <1643621827@qq.com>
2019-12-04 10:17:51 +0800

    迪米特法则（最少知道原则）

---
+++ src/main/java/com/example/demo/softwaredesign/Boss.java
@@ -0,0 +1,14 @@
+package com.example.demo.softwaredesign;
+
+/**
+ * @ClassName : Boss
+ * @Description: TODO
+ * @Author: jilvke
+ * @CreateDate: 2019/12/04
+ * @Version: 1.0
+ */
+public class Boss {
+    public void commandCheckNumber(TeamLeader teamLeader) {
+        teamLeader.checkNumberOfProject();
+    }
+}
